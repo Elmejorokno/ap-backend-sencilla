e5932bfdb3ff2c91a3bc093540687d50a6053d89
Ferdy Daniel Iglesias <ferdyiglesias12@gmail.com>
2022-04-02 13:36:48 -0600

    heroku config

--- index.js
+++ index.js
@@ -76,5 +76,5 @@ app.post('/api/notes', (req, res) => {
 app.use((req, res) => {
   res.status(404).json({ error: { msg: 'Page not found.' } })
 })
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`))
